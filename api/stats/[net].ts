82020cfebdae9f84d55bd0f57cd20e49dc3ba35b
Norbert Bodziony <norbertbodziony@gmail.com>
2021-09-20 15:10:43 +0200

    fix cors

--- api/stats/[net].ts
+++ api/stats/[net].ts
@@ -3,17 +3,27 @@ import DEVNET_DATA from '../../data/devnet/stats.json'
 import TESTNET_DATA from '../../data/devnet/stats.json'
 import MAINNET_DATA from '../../data/devnet/stats.json'
 export default function (req: VercelRequest, res: VercelResponse) {
+  //@ts-expect-error
+  res.setHeader('Access-Control-Allow-Credentials', true)
+  res.setHeader('Access-Control-Allow-Origin', '*')
+  // res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
+  res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
+  )
+
   const { net } = req.query
   if (net === 'devnet') {
-    res.setHeader('Access-Control-Allow-Origin', '*').json(DEVNET_DATA)
+    res.json(DEVNET_DATA)
     return
   }
   if (net === 'testnet') {
-    res.setHeader('Access-Control-Allow-Origin', '*').json(TESTNET_DATA)
+    res.json(TESTNET_DATA)
     return
   }
   if (net === 'mainnet') {
-    res.setHeader('Access-Control-Allow-Origin', '*').json(MAINNET_DATA)
+    res.json(MAINNET_DATA)
     return
   }
   res.status(400).send('INVALID NETWORK')
