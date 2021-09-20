13be3e28c1813b7ad8d9e495e89ca802b0c755e7
Norbert Bodziony <norbertbodziony@gmail.com>
2021-09-20 15:06:47 +0200

    fix cors

--- api/stats/[net].ts
+++ api/stats/[net].ts
@@ -3,18 +3,17 @@ import DEVNET_DATA from '../../data/devnet/stats.json'
 import TESTNET_DATA from '../../data/devnet/stats.json'
 import MAINNET_DATA from '../../data/devnet/stats.json'
 export default function (req: VercelRequest, res: VercelResponse) {
-  res.setHeader('Access-Control-Allow-Origin', '*')
   const { net } = req.query
   if (net === 'devnet') {
-    res.json(DEVNET_DATA)
+    res.setHeader('Access-Control-Allow-Origin', '*').json(DEVNET_DATA)
     return
   }
   if (net === 'testnet') {
-    res.json(TESTNET_DATA)
+    res.setHeader('Access-Control-Allow-Origin', '*').json(TESTNET_DATA)
     return
   }
   if (net === 'mainnet') {
-    res.json(MAINNET_DATA)
+    res.setHeader('Access-Control-Allow-Origin', '*').json(MAINNET_DATA)
     return
   }
   res.status(400).send('INVALID NETWORK')
